24175f8672debe4d88633f4bd034fdd2d30f47a9
PatrykLazuk <patryk.lazuk@gmail.com>
2021-08-10 22:05:46 +0200

    Added controller for Country and Hotel

---
+++ HotelListing/Controllers/CountryController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using HotelListing.IRepository;
+using HotelListing.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<CountryController> _logger;
+        private readonly IMapper _mapper;
+
+        public CountryController(ILogger<CountryController> logger, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCountries()
+        {
+            try
+            {
+                var coutries = await _unitOfWork.Countries.GetAll();
+                var results = _mapper.Map<IList<CountryDTO>>(coutries);
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex,$"Something went wrong in the {nameof(GetCountries)}");
+                return StatusCode(500, "Internal Server Error, try again later");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCountry(int id)
+        {
+            try
+            {
+                var country = await _unitOfWork.Countries.Get(q => q.Id == id,new List<string> { "Hotels" });
+                var results = _mapper.Map<CountryDTO>(country);
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something went wrong in the {nameof(GetCountry)}");
+                return StatusCode(500, "Internal Server Error, try again later");
+            }
+        }
+    }
+}
---
+++ HotelListing/Controllers/HotelController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using HotelListing.IRepository;
+using HotelListing.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<HotelController> _logger;
+
+        public HotelController(IUnitOfWork unitOfWork, IMapper mapper, ILogger<HotelController> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _logger = logger;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetHotels()
+        {
+            try
+            {
+                var hotels = await _unitOfWork.Hotels.GetAll();
+                var result = _mapper.Map<IList<HotelDTO>>(hotels);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error in {nameof(GetHotels)}");
+                return StatusCode(500, "Server Error, please try again later.");
+            }
+
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetHotel(int id)
+        {
+            try
+            {
+                var hotel = await _unitOfWork.Hotels.Get(x => x.Id == id, new List<string> { "Country" });
+                var result = _mapper.Map<HotelDTO>(hotel);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error in {nameof(GetHotel)}");
+                return StatusCode(500, "Server Error, please try again later.");
+            }
+        }
+
+
+    }
+}
